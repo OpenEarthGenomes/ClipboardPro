c7c3d20e4008c57f312935b1bf1d989ef5248c19
Fáber Sándor <sandorwow@gmail.com>
2025-12-30 01:55:26 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
+// Csak ennyi kell ide
 plugins {
-    // Csak deklaráljuk a verziót, de az 'apply false' kell ide!
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
